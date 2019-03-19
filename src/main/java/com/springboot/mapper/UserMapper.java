5c779e68f06fe91b0db8914e3fd3dda1d662b258
Gruuy <245746119@qq.com>
2019-03-19 22:08:41 +0800

    增加整合了PageHelper以及一些简单说明

--- src/main/java/com/springboot/mapper/UserMapper.java
+++ src/main/java/com/springboot/mapper/UserMapper.java
@@ -3,7 +3,10 @@ package com.springboot.mapper;
 import com.springboot.Model.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     User getUser(Integer id);
+    List<User> getAllUser();
 }
--- src/test/java/com/springboot/MobanApplicationTests.java
+++ src/test/java/com/springboot/MobanApplicationTests.java
@@ -1,5 +1,10 @@
 package com.springboot;
 
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.springboot.Model.User;
+import com.springboot.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +12,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.JedisPool;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MobanApplicationTests {
     //自动注入返回值为JedisPool的对象
     @Autowired
     JedisPool jedisPool;
+
+    @Autowired
+    UserMapper userMapper;
     @Test
-    public void contextLoads() {
+    public void testPageHelper() {
+        PageHelper.startPage(1,1);
+        List<User> list=userMapper.getAllUser();
+        PageInfo<User> page=new PageInfo<User>(list);
+        System.out.println(page.getList().get(0).getId());
+        System.out.println(page.getList().get(0).getUsername());
+        System.out.println(page.getList().get(0).getPassword());
+        System.out.println(page.getList().get(0).getLevel());
     }
     @Test
     public void testJedis(){
